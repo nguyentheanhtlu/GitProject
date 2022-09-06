726d1c82720a03353b207ee171c304bb5c6e583c
nguyentheanhtlu <theanh28022003@gmail.com>
2022-09-06 16:05:14 +0700

    bai1

---
+++ TOANTU/JSbai1.js
@@ -0,0 +1,10 @@
+
+let a=parseInt(prompt("diem vat ly"));
+let b=parseInt(prompt("diem hoa hoc"));
+let c=parseInt(prompt("diem sinh hoc"));
+let d =(a+b+c)/3;
+alert(d);
+let f=a+b+c;
+alert(f);
+
+
