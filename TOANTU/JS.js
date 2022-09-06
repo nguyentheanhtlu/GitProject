0f186810a7b9c0f1d127076ca55a78a1cf20f7df
nguyentheanhtlu <theanh28022003@gmail.com>
2022-09-06 15:40:47 +0700

    baitap2

---
+++ TOANTU/JS.js
@@ -0,0 +1,9 @@
+let inputWight;
+let inputHeight;
+
+inputWight=prompt("enter the width");
+inputHeight=prompt("enter the height");
+let width = parseInt(inputWight);
+let height= parseInt(inputHeight);
+let area = width * height;
+document.write("the area is: "+ area);
