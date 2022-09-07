386c88e39d98d5e8d1c16da7ecf5f00e300631cb
nguyentheanhtlu <theanh28022003@gmail.com>
2022-09-07 12:00:25 +0700

    maytinh

---
+++ TOANTU/JSmaytinh.js
@@ -0,0 +1,28 @@
+function Addition() {
+
+    let giatrivao1 = +document.getElementById('nhapgiatri1').value;
+    let giatrivao2 = +document.getElementById('nhapgiatri2').value;
+    let a=giatrivao1 + giatrivao2;
+    alert(a);
+}
+function Subtraction(){
+
+    let giatrivao1 = document.getElementById('nhapgiatri1').value;
+    let giatrivao2 = document.getElementById('nhapgiatri2').value;
+    let b=giatrivao1 - giatrivao2;
+    alert(b);
+}
+function Multiplication(){
+
+    let giatrivao1 = document.getElementById('nhapgiatri1').value;
+    let giatrivao2 = document.getElementById('nhapgiatri2').value;
+    let c=giatrivao1 * giatrivao2;
+    alert(c);
+}
+function Division(){
+
+    let giatrivao1 = document.getElementById('nhapgiatri1').value;
+    let giatrivao2 = document.getElementById('nhapgiatri2').value;
+    let d=giatrivao1 / giatrivao2;
+    alert(d);
+}
